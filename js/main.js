4291415796bac5b4914385f2ab329b8ecf9e5ced
Geovane Maia <dartanha272@gmail.com>
2020-12-06 22:55:59 -0300

    conceitos basicos em JS

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// var d = new Date;
+// alert(d);
+// alert(d.getDay);
+// alert(d.getHours);
+
+// var contador;
+// for(contador = 0; contador <= 5; contador++){
+// alert(contador);
+// };
+
+// var count = 0;
+// while(count <= 5){
+//     console.log(count);
+//     alert(count);
+//     count ++;
+// };
+
+// var idade = prompt("Qual a sua idade ?");
+// if(idade >= 18){
+//     alert("Maior de idade !!");
+// }else{
+//     alert("Menor de idade !!");
+// };
+
+// var fruta = {nome:"maça",cor:"vermelho"};
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var nome = "Geovane Maia";
+// var idade = 27;
+// var idade2 = 10;
+// //alert(nome + " Tem " + idade + "anos");
+// alert(idade + idade2);
+
+// var lista = ["Maça","Pera","Laranja"];
+// //lista.push("Uva");
+// //lista.pop();
+// console.log(lista);
+// alert(lista);
